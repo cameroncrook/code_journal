5813fd2162014f43399912d67b4ea9c54483ba1e
cameroncrook <cameronbcrook@gmail.com>
2023-07-14 00:18:58 -0600

    Auto generate links

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,15 @@
 function build_nav(nav_list) {
     let nav = document.querySelector('header nav');
+    const heading = document.querySelector('h1');
 
     nav_list.forEach(item => {
         let li = document.createElement('li');
         let a = document.createElement('a');
-        a.href = `views/${item}.html`;
+        if (heading.textContent == 'My Coding Journal') {
+            a.href = `views/${item}.html`;
+        } else {
+            a.href = `${item}.html`;
+        }
         a.textContent = item.toUpperCase();
 
         li.appendChild(a);
@@ -12,6 +17,24 @@ function build_nav(nav_list) {
     });
 }
 
+function build_contents_table() {
+    const sections = document.querySelectorAll('h2');
+
+    let table = document.querySelector('#contents-table');
+
+    sections.forEach(item => {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        a.href = `#${item.id}`;
+        a.text = item.textContent;
+
+        li.appendChild(a);
+        table.appendChild(li);
+    });
+}
+
 const nav = ['css', 'flutter', 'html', 'javascript', 'python', 'react_native', 'react', 'sql']
 
 build_nav(nav);
+
+build_contents_table();
